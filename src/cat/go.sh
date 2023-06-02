00b8da89e47070c9fe87bc35482cf3394079a049
kamil116rus <kh8@rambler.ru>
2023-06-02 21:35:25 +0300

    sh

---
+++ src/cat/go.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+
+parametr1=$1
+
+parametr2=$2
+
+script_name=$0 #присваиваем переменной script_name значение имени скрипта
+
+echo "Вы запустили скрипт с именем $script_name и параметром $parametr1 и $parametr2"
+
+clang-format -i *.c *.h
+
+git add . 
+git commit -m"$parametr1 $parametr2 $3 $4 $5 $6 $7"
+git push origin main
+
+#gcc -Wall -Werror -Wextra 
--- src/cat/k_cat.h
+++ src/cat/k_cat.h
@@ -4,5 +4,4 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 #endif

a073fe6cea2f800deb47ea1021707f3b6c7498d2
kamil116rus <kh8@rambler.ru>
2023-06-02 22:16:23 +0300

    sh

--- src/cat/cat.c
+++ src/cat/cat.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
 
-int main(int argc, char *argv) { retutn 0; }
+void print_file(char filename);
+
+int main(int argc, char** argv) {
+    for (int i = 1; i < argc; i++) {
+        print_file(argv[i]);
+    }
+    return 0;
+}
+
+void print_file(char filename) {
+    FILE* file = fopen(filename, "r");
+    if (file != NULL) {
+        char c = getchar();
+        while (c != EOF) {
+            putchar(c);
+            c = getchar();
+        }
+    }
+}
--- src/cat/k_cat.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv) {
-    if (argc > 1) {
-        FILE *file;
-        file = fopen(argv, r);
-    }
-    FILE *file retutn 0;
-}

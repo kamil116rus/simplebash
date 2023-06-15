ff8edf70b82e522a3421b2ed750dd82d22b210c7
kamil116rus <kh8@rambler.ru>
2023-06-15 21:31:13 +0300

    flag b flag e flag n

--- src/cat/cat.c
+++ src/cat/cat.c
@@ -2,26 +2,34 @@
 
 #include <stdio.h>
 
-void print_file(char* filename);
+void print_file(char* filename, options* option);
 int parser_option(int argc, char** argv, options* option);
 
 int main(int argc, char** argv) {
     options option = {0};
     int file_number;
     file_number = parser_option(argc, argv, &option);
-    for (int i = 1; i < argc; i++) {
-        print_file(argv[i]);
+    for (; file_number < argc; file_number++) {
+        print_file(argv[file_number], &option);
     }
     return 0;
 }
 
-void print_file(char* filename) {
+void print_file(char* filename, options* option) {
     FILE* file = fopen(filename, "r");
     if (file != NULL) {
-        char c = fgetc(file);
-        while (c != EOF) {
+        char prev;
+        for (char c = prev = '\n'; (c = fgetc(file)) != EOF; prev = c) {
+            if (option->flag_e == 1 && option->flag_v == 1 && c == '\n') printf("$");
+            if (option->flag_n != 0 && prev == '\n') {
+                printf("%6d\t", option->flag_n);
+                option->flag_n++;
+                option->flag_b == 0;
+            }
+            if (option->flag_b != 0 && prev == '\n' && c != '\n') printf("%6d\t", option->flag_b);
+            if (option->flag_b != 0 && c == '\n' && prev != '\n') option->flag_b++;
+
             putchar(c);
-            c = fgetc(file);
         }
     }
 }
@@ -33,15 +41,15 @@ int parser_option(int argc, char** argv, options* option) {
                                            {0, 0, 0, 0}};
     int flag = 0;
 
-    while (flag = getopt_long(argc, argv, "AbeEnstTv", long_options, NULL) != -1) {
+    while ((flag = getopt_long(argc, argv, "AbeEnstTv", long_options, NULL)) != -1) {
         if (flag == 'A')
             option->flag_v = option->flag_e = option->flag_t = 1;
         else if (flag == 'b')
             option->flag_b = 1;
         else if (flag == 'e')
-            option->flag_e = option->flag_v = 1;
-        else if (flag == 'E')
             option->flag_e = 1;
+        else if (flag == 'E')
+            option->flag_e = option->flag_v = 1;
         else if (flag == 'n')
             option->flag_n = 1;
         else if (flag == 's')

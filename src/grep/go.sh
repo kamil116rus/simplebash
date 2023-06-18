80b0d75f66e2f82fac2d6da58ddb8f554fccaccc
kamil116rus <kh8@rambler.ru>
2023-06-18 23:43:34 +0300

    init grep.c rec

--- src/grep/go.sh
+++ src/grep/go.sh
@@ -7,11 +7,11 @@ parametr2=$2
 
 script_name=$0 #присваиваем переменной script_name значение имени скрипта
 
-echo "Вы запустили скрипт с именем $script_name и параметрами $@"
+echo "Вы запустили скрипт с именем $script_name и параметр $@"
 clang-format -i *.c *.h
 
 git add . 
-git commit -m"$parametr1 $parametr2 $3 $4 $5 $6 $7"
+git commit -m"$parametr1 $2 $3 $4 $5 $6 $7"
 git push origin main
 
 #gcc -Wall -Werror -Wextra 

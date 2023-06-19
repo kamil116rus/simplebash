3b6a972af2ca44f4cac1f2c3970606e5bfc98656
kamil116rus <kh8@rambler.ru>
2023-06-19 23:35:25 +0300

    intro

--- src/grep/grep.c
+++ src/grep/grep.c
@@ -3,4 +3,5 @@
 
 int main(int argc, char **argv) {
     grep_programm(argc, argv);
-    return 0; }
+    return 0;
+}
--- src/grep/grep.h
+++ src/grep/grep.h
@@ -19,6 +19,7 @@ typedef struct {
     int error;
 } options;
 
+void grep_programm(int argc, char **argv);
 void print_file(char* filename, options* option);
 int parser_option(int argc, char** argv, options* option);
 

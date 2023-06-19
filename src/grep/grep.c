4007db895aa4acfd7fdb8a0ef25ff4625350317b
kamil116rus <kh8@rambler.ru>
2023-06-19 23:36:02 +0300

    intro

--- src/grep/grep.c
+++ src/grep/grep.c
@@ -2,6 +2,6 @@
 #include "grep.h"
 
 int main(int argc, char **argv) {
-    grep_programm(argc, argv);
-    return 0;
+  grep_programm(argc, argv);
+  return 0;
 }
--- src/grep/grep.h
+++ src/grep/grep.h
@@ -6,20 +6,20 @@
 #include <stdlib.h>
 
 typedef struct {
-    int flag_e;
-    int flag_i;
-    int flag_v;
-    int flag_c;
-    int flag_l;
-    int flag_n;
-    int flag_h;
-    int flag_s;
-    int flag_f;
-    int flag_o;
-    int error;
+  int flag_e;
+  int flag_i;
+  int flag_v;
+  int flag_c;
+  int flag_l;
+  int flag_n;
+  int flag_h;
+  int flag_s;
+  int flag_f;
+  int flag_o;
+  int error;
 } options;
 
-void grep_programm(int argc, char **argv);
+void grep_programm(int argc, char** argv);
 void print_file(char* filename, options* option);
 int parser_option(int argc, char** argv, options* option);
 
